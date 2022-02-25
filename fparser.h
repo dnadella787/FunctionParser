ac67ca53668e31519bcf609f5985165dabd45de3
Dhanush Nadella <dnadella787@gmail.com>
2022-02-24 16:43:04 -0800

    multidigit funcs done

--- fparser.h
+++ fparser.h
@@ -17,9 +17,9 @@ class fparser
 protected:
     static_assert(std::is_arithmetic<T>::value, "fparser template parameter must be numeric (int, float, long, etc).");
 
-    std::string func;
-    std::string postfix;
-    std::string var; 
+    std::string ifix_func;
+    std::string pfix_func;
+    std::string eval_var; 
 
 
     std::map<char, T (*) (T x, T y)> operators;
@@ -48,7 +48,7 @@ protected:
 
 
 public:
-    fparser(const std::string& f, const std::string& input_var) : func(f), var(input_var)
+    fparser(const std::string& f, const std::string& input_var) : ifix_func(f), eval_var(input_var)
     {
         //first initialize all the operators and their lambdas 
         operators['+'] = [](T x, T y){return x + y;};
@@ -59,22 +59,22 @@ public:
 
         //produce the function in infix notation to perform calculations
         std::stack<char> f_stack;
-        for (std::string::const_iterator it = f.begin(); it != f.end(); ++it)
+        for (size_t i = 0; i < f.size(); ++i)
         {
-            if (*it == ' ')
+            if (f[i] == ' ')
                 continue; 
 
-            else if (isalnum(*it))
-                postfix += *it;
+            else if (isalnum(f[i]))
+                pfix_func += f[i];
 
-            else if (*it == '(' || *it == '^')
-                f_stack.push(*it);
+            else if (f[i] == '(' || f[i] == '^')
+                f_stack.push(f[i]);
 
-            else if (*it == ')')
+            else if (f[i] == ')')
             {
                 while (!f_stack.empty() && f_stack.top() != '(')
                 {
-                    postfix += f_stack.top();
+                    pfix_func += f_stack.top();
                     f_stack.pop();
                 }
                 f_stack.pop();
@@ -82,43 +82,42 @@ public:
 
             else
             {
-                if (preced(*it) > preced(f_stack.top()))
-                    f_stack.push(*it);
+                if (preced(f[i]) > preced(f_stack.top()))
+                    f_stack.push(f[i]);
             
                 else
                 {
-                    while (!f_stack.empty() && preced(*it) <= preced(f_stack.top()))
+                    while (!f_stack.empty() && preced(f[i]) <= preced(f_stack.top()))
                     {
-                        postfix += f_stack.top();
+                        pfix_func += f_stack.top();
                         f_stack.pop();
                     }
-                    f_stack.push(*it);
+                    f_stack.push(f[i]);
                 }
             }
         }
 
         while (!f_stack.empty())
         {
-            postfix += f_stack.top();
+            pfix_func += f_stack.top();
             f_stack.pop();
         }
     }
 
 
-    // this currently works but need to make it so you can have 
-    // multiple digits for coefficients (like 32*x)
-    // also needs to be refactored to use iterators instead of 
-    // just an int
-    T calculate(const T x_val)
+    //gives a y value given a specifici x value, only a single 
+    //value, to do multuple input a vector, overloaded func is 
+    //below 
+    T calculate(const T& x_val)
     {
         T y_val;
         std::stack<T> s;
 
-        for (int i = 0; i < postfix.size(); ++i)
+        for (int i = 0; i < pfix_func.size(); ++i)
         {
-            auto iter = operators.find(postfix[i]);
+            auto iter = operators.find(pfix_func[i]);
 
-            //if the current char in the postfix string is an operator
+            //if the current char in the pfix_func string is an operator
             if (iter != operators.end())
             {
                 assert(s.size() >= 2);
@@ -133,15 +132,39 @@ public:
             else
             {
                 // if its not an operator, check if its a variable
-                if (postfix.substr(i,1) == var)
+                if (pfix_func.substr(i,1) == eval_var)
                     s.push(x_val);
                 // otherwise it is a constant just add to stack
                 else
-                    s.push(convert_to(postfix.substr(i,1)));
+                {
+                    std::string curr_dig = "";
+                    while (std::isdigit(pfix_func[i]))
+                    {
+                        curr_dig += pfix_func[i];
+                        ++i;
+                    }
+                    --i;
+
+                    s.push(convert_to(curr_dig));
+                }
             }
         }
 
         return s.top();
+    }
+
+
+    //overloaded version of calculate that takes in multiple x vals
+    //returns vector of all outputted y values in order 
+    std::vector<T> calculate(const std::vector<T>& x_vals)
+    {
+        std::vector<T> y_vals(x_vals.size());
+        for (size_t i = 0; i < x_vals.size(); ++i)
+        {
+            y_vals[i] = calculate(x_vals[i]);
+        }
+
+        return y_vals;
     }
 };
 
--- test.cpp
+++ test.cpp
@@ -1,40 +1,15 @@
 #include <iostream>
 #include <string>
-#include <map>
+#include <vector>
 #include "fparser.h"
 
-
-// class tester
-// {
-//     std::map<char, double (*)(double x, double y)> operators;
-
-// public:
-//     tester(std::string s)
-//     {
-//         operators['+'] = [](double x, double y){return x + y;};
-//         for (int i = 0; i < s.size(); i++)
-//         {
-//             std::map<char, double (*)(double x, double y)>::iterator iter = operators.find(s[i]);
-//             if (iter == operators.end())
-//             {
-//                 continue;
-//             }
-//             else 
-//             {
-//                 auto func = iter->second;
-//                 int x = std::stoi(s.substr(i-1, 1));
-//                 int y = std::stoi(s.substr(i-2, 1));
-
-//                 std::cout << func(x, y) << std::endl;
-//             }
-//         }
-//     }
-// };
 
 
 int main()
 {
-    // tester("a12+");
-    fparser::fparser<double> fp("x^2 + 2*x + 3", "x");
+    fparser::fparser<double> fp("x^x - 23*x + 3", "x");
     std::cout << fp.calculate(1.25) << std::endl;
+    auto out = fp.calculate({1,2,3,4});
+    for (auto y : out)
+        std::cout << y << std::endl;
 }
